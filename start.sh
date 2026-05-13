b76a0adc7829d583aa8f81f35457e28c208c6d23
ritikkumar1233 <ritikroushan96905@gmail.com>
2026-05-13 19:53:52 +0530

    Fix start.sh line endings

--- start.sh
+++ start.sh
@@ -14,4 +14,4 @@ php artisan optimize
 
 echo "Starting Laravel server..."
 
-php artisan serve --host=0.0.0.0 --port=10000 (See <attachments> above for file contents. You may not need to search or read the file again.)
+php artisan serve --host=0.0.0.0 --port=10000
